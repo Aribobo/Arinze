47641e8f5eeaf9da9199a96ae7d8f3400ab12530
Aribobo <stanleyemmanuel04@gmail.com>
2023-06-05 08:01:48 +0100

    img bug

--- pages/components/Project.jsx
+++ pages/components/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
   return (
     <div id="project" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-2xl tracking items-center uppercase text-[#fd7e14]">
+        <p className="text-xl md:text-2xl tracking items-center uppercase text-[#fd7e14]">
           Projects
         </p>
         <h2 className="py-4"> What I&apos;ve Built</h2>
@@ -14,10 +14,10 @@ const Project = () => {
             title="Iphoundit"
             backgroundImg="iphoundit.png"
             projectUrl="Property"
-          />-
+          />
            <Projectitems
             title="Lucid-Dashboard"
-            backgroundImg="css.png"
+            backgroundImg="board.png"
             projectUrl="Dashboard"
           />
         </div>
